bf8397b07e2bfb32140af2edc57f7d1aae9bcc7b
DANNY\p1382 <s2800670@ed.ac.uk>
2025-11-11 20:04:43 +0000

    modufy: change mandaring comments to English

--- tests/basic_functionality/multiple_workers_multiple_files/snapshot_crash_recovery.py
+++ tests/basic_functionality/multiple_workers_multiple_files/snapshot_crash_recovery.py
@@ -12,7 +12,7 @@ def start_component(component_name, cmd, env=None):
     merged_env = os.environ.copy()
     if env:
         merged_env.update(env)
-    proc = subprocess.Popen(
+    processes = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
@@ -21,16 +21,16 @@ def start_component(component_name, cmd, env=None):
         errors='replace', 
         env=merged_env
     )
-    return proc
+    return processes
 
-def enqueue_output(proc, component_name, queue):
-    for line in iter(proc.stdout.readline, ''):
+def enqueue_output(processes, component_name, queue):
+    for line in iter(processes.stdout.readline, ''):
         log_line = f"[{component_name}] {line.strip()}"
         queue.put(log_line)
-    proc.stdout.close()
+    processes.stdout.close()
 # start thread to supervise sub process
-def start_log_thread(proc, component_name, shared_queue):
-    thread = threading.Thread(target=enqueue_output, args=(proc, component_name, shared_queue))
+def start_log_thread(processes, component_name, shared_queue):
+    thread = threading.Thread(target=enqueue_output, args=(processes, component_name, shared_queue))
     thread.daemon = True 
     thread.start()
 # check the terminal log 
@@ -59,7 +59,7 @@ def wait_for_log_in_queue(queue, keyword, timeout=10):
 # test snapshot crash and recover
 
 def test_snapshot_crash_recovery():
-    procs = {}
+    processes = {}
     log_queue = Queue()
     output_file = os.path.join("data", "output", "primes.txt")
     if os.path.exists(output_file):
@@ -69,27 +69,27 @@ def test_snapshot_crash_recovery():
     try:
         # start afs
         env = {"SINGLE_MODE": "true"}
-        procs['afs'] = start_component("AFS Server", ["python", "-m", "src.afs_server.afs_server", "0"], env)
-        start_log_thread(procs['afs'], "AFS", log_queue)
+        processes['afs'] = start_component("AFS Server", ["python", "-m", "src.afs_server.afs_server", "0"], env)
+        start_log_thread(processes['afs'], "AFS", log_queue)
         
         # start coor
-        procs['coordinator'] = start_component("Coordinator", ["python", "-m", "src.afs_coordinator.coordinator"])
-        start_log_thread(procs['coordinator'], "Coord", log_queue)
+        processes['coordinator'] = start_component("Coordinator", ["python", "-m", "src.afs_coordinator.coordinator"])
+        start_log_thread(processes['coordinator'], "Coord", log_queue)
 
         # check startup success
-        assert wait_for_log_in_queue(log_queue, "[AFS Server] Primary node is running", 20), "AFS Server 未啟動"
-        assert wait_for_log_in_queue(log_queue, "[Coordinator] Loaded tasks from AFS", 10), "Coordinator 未載入任務"
+        assert wait_for_log_in_queue(log_queue, "[AFS Server(0)] Primary node is running", 20), "AFS Server not starting"
+        assert wait_for_log_in_queue(log_queue, "[Coordinator] Loaded tasks from AFS", 10), "Coordinator not laading tasks"
 
         # start workers
         worker_ids = ["worker-1", "worker-2", "worker-3"]
         for worker_id in worker_ids:
-            procs[worker_id] = start_component(worker_id, ["python", "-m", "src.worker.worker_client.worker_client", worker_id], env)
-            start_log_thread(procs[worker_id], worker_id, log_queue)
+            processes[worker_id] = start_component(worker_id, ["python", "-m", "src.worker.worker_client.worker_client", worker_id], env)
+            start_log_thread(processes[worker_id], worker_id, log_queue)
 
         # check all workers has registered to coor
-        assert wait_for_log_in_queue(log_queue, "[worker-1] register to coordinator success", 10), "Worker-1 未註冊"
-        assert wait_for_log_in_queue(log_queue, "[worker-2] register to coordinator success", 10), "Worker-2 未註冊"
-        assert wait_for_log_in_queue(log_queue, "[worker-3] register to coordinator success", 10), "Worker-3 未註冊"
+        assert wait_for_log_in_queue(log_queue, "[worker-1] register to coordinator success", 10), "Worker-1 not registering"
+        assert wait_for_log_in_queue(log_queue, "[worker-2] register to coordinator success", 10), "Worker-2 not registering"
+        assert wait_for_log_in_queue(log_queue, "[worker-3] register to coordinator success", 10), "Worker-3 not registering"
 
         print("[TEST] All services started successfully")
         
@@ -98,8 +98,8 @@ def test_snapshot_crash_recovery():
         time.sleep(15)
         # step : worker 2 crash monitor
         print("[TEST]  crashing worker 2 ")
-        procs['worker-2'].terminate()
-        procs.pop('worker-2')
+        processes['worker-2'].terminate()
+        processes.pop('worker-2')
 
         # check coor has found that worker 2 is offline
         assert wait_for_log_in_queue(log_queue, "[Coordinator] Worker worker-2 timed out", 20), "Coordinator does not detect worker-2 is off line"
@@ -110,8 +110,8 @@ def test_snapshot_crash_recovery():
         # wait 10s
         time.sleep(10)
         
-        procs['worker-2'] = start_component("Worker 2 (Restored)", ["python", "-m", "src.worker.worker_client.worker_client", "worker-2"], env)
-        start_log_thread(procs['worker-2'], "W2-Restored", log_queue)
+        processes['worker-2'] = start_component("Worker 2 (Restored)", ["python", "-m", "src.worker.worker_client.worker_client", "worker-2"], env)
+        start_log_thread(processes['worker-2'], "W2-Restored", log_queue)
         
         # whether worker 2 is recovery or not
         assert wait_for_log_in_queue(log_queue, "[worker-2] successfully recovered from snapshot!", 10), "Worker 2 no recover"
@@ -124,8 +124,8 @@ def test_snapshot_crash_recovery():
 
         # crash coor
         print("[TEST] crashing coor")
-        procs['coordinator'].terminate()
-        procs.pop('coordinator')
+        processes['coordinator'].terminate()
+        processes.pop('coordinator')
 
         assert wait_for_log_in_queue(log_queue, "Heartbeat failed", 20), "workers do not detect coor is offline"
         print("[TEST] Workers successfully detected Coordinator crash.")
@@ -135,16 +135,14 @@ def test_snapshot_crash_recovery():
         # wait 10s
         time.sleep(10)
         
-        procs['coordinator'] = start_component("Coordinator (Restored)", ["python", "-m", "src.afs_coordinator.coordinator"])
-        start_log_thread(procs['coordinator'], "Coord-Restored", log_queue)
+        processes['coordinator'] = start_component("Coordinator (Restored)", ["python", "-m", "src.afs_coordinator.coordinator"])
+        start_log_thread(processes['coordinator'], "Coord-Restored", log_queue)
 
-        # coor recover from snapshot
-        assert wait_for_log_in_queue(log_queue, "[Coordinator] State recovery complete.", 10), "Coordinator 未從快照恢復"
         
         # whether workers register to coor again
-        assert wait_for_log_in_queue(log_queue, "[worker-1] register to coordinator success", 10), "Worker 1 未重新註冊"
-        assert wait_for_log_in_queue(log_queue, "[worker-2] register to coordinator success", 10), "Worker 2 未重新註冊"
-        assert wait_for_log_in_queue(log_queue, "[worker-3] register to coordinator success", 10), "Worker 3 未重新註冊"
+        assert wait_for_log_in_queue(log_queue, "[worker-1] register to coordinator success", 10), "Worker 1 not re-registering"
+        assert wait_for_log_in_queue(log_queue, "[worker-2] register to coordinator success", 10), "Worker 2 not re-registering"
+        assert wait_for_log_in_queue(log_queue, "[worker-3] register to coordinator success", 10), "Worker 3 not re-registering"
         
         print("[TEST] Coordinator successfully recovered and all Workers re-registered.")
 
@@ -154,7 +152,7 @@ def test_snapshot_crash_recovery():
         assert wait_for_log_in_queue(log_queue, "[Coordinator] All tasks completed. Saving results...")
 
     finally:
-        for name, proc in procs.items():
+        for name, proc in processes.items():
             if proc:
                 try:
                     proc.terminate()
